28177d46f6fc854a931eab287a046652efd6b674
Alexandre Cordani <alcordani@gmail.com>
2016-03-28 10:49:17 +0200

    photos suite

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,44 @@
+class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+
+
+
+  def flat_params
+  params.require(:flat).permit(
+      :first_name,
+      :last_name,
+      :surface_land,
+      :surface_habitation,
+      :common_wall,
+      :rooms,
+      :bedroom,
+      :bathroom,
+      :surface_carrez,
+      :year_construction,
+      photos: [])
+  end
+
+
+
+end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,3 @@
 class Flat < ActiveRecord::Base
+  has_attachments :photos, maximum: 3
 end
