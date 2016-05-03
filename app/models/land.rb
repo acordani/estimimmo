dc1c97388ca601a432f1d6b10d9e958aa8657eae
ac <alcordani@gmail.com>
2016-05-03 22:34:14 +0200

    address_complete in model

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,11 +1,16 @@
 class Land < ActiveRecord::Base
 
 	extend FriendlyId
-	friendly_id :address, use: :slugged
+	friendly_id :address_complete, use: :slugged
 
 	def self.import(file)
     	CSV.foreach(file.path, headers: true) do |row|
       		Land.create! row.to_hash
      	end
   	end
+
+  	def address_complete
+  		[:address, :city]
+  	end
+
 end
